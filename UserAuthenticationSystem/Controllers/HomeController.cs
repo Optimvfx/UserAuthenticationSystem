4a916920bdd4aa2e14f23657a9696f00a315401c
Maxim Sidorenko <94392000+Optimvfx@users.noreply.github.com>
2023-02-12 04:29:44 +0200

    Initial

---
+++ UserAuthenticationSystem/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using UserAuthenticationSystem.Models;
+
+namespace UserAuthenticationSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+     
+        [Authorize(Policy = "OnlyForMicrosoft")]
+        public IActionResult About()
+        {
+            return Content("Only for Microsoft employees");
+        }
+    }
+}
